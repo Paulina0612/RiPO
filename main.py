88ed29a2ccc7bb7df637d5de1d3fada3b53d2f78
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-04-06 00:58:32 +0200

    Todo added

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ base_image = cv2.imread('photos\\photo.jpg')
 live_face_detection.live_face_detection(base_image, video_path='')
 
 
-# TODO: Trzeba jeszce dodac rysowanie landmarkow na twarzy
+# TODO: Trzeba jeszce zrobić interfejs, żeby można było wybrać zdjęcie, video lub kamerę (imo narazie w konsoli)
 
